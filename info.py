1bc0c0509a90cd42144ff715fe46d82e4e9a902e
backbencher-bannu <harishwhatsapp0306@gmail.com>
2025-01-23 21:12:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ BOT_USERNAME = environ.get("BOT_USERNAME", 'ynvmoviesbot') # without @
 
 # Admins, Channels & Users
 BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-100218550999')) # admin your channel in stream 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-100218550999')) # admin your channel in users log 
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1002290275432')) # admin your channel in users log 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1989461858').split()] # 3567788, 678899, 5889467
 
 # pics information
